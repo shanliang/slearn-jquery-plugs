6fa222ab7119a7bff6d913907caf5619a3f4cf4d
shanliang <sandycoding@163.com>
2014-11-18 00:16:23 +0800

    first commit

---
+++ plugs/Layer/src/jquery.layer.js
@@ -0,0 +1,27 @@
+/*
+* author sandy liang
+* date 2014-11-17
+*/
+;(function($){
+	function Layer(){
+
+	}
+
+	$.extend(Layer.prototype,{		
+		show: function(){
+
+		},
+		close: function(){
+
+		},
+		position: function(){
+
+		}
+	});
+
+	$.fn.layer = function(config){
+		return function(){};
+	};
+
+})(jQuery);
+
